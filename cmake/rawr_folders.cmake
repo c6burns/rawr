60e83549743cc6c4bfa4eeca9e6efe93196c892f
c6burns <chris@kitty.city>
2020-12-31 12:11:08 -0700

    added additional organization

--- cmake/rawr_folders.cmake
+++ cmake/rawr_folders.cmake
@@ -52,6 +52,7 @@ rawr_target_folder(test_opus_padding "tests/opus")
 # libsrtp
 rawr_target_folder(srtp2 "deps")
 rawr_target_folder(srtp_driver "examples/libsrtp")
+rawr_target_folder(rtpw "examples/libsrtp")
 rawr_target_folder(test_srtp "tests/libsrtp")
 
 # strophe
@@ -64,6 +65,7 @@ rawr_target_folder(component "examples/strophe")
 rawr_target_folder(register "examples/strophe")
 rawr_target_folder(roster "examples/strophe")
 rawr_target_folder(uuid "examples/strophe")
+rawr_target_folder(strophe-xep_example "examples/strophe-xep")
 
 # portaudio
 rawr_target_folder(portaudio "deps")
@@ -89,3 +91,24 @@ endif()
 # libuv
 rawr_target_folder(uv_a "deps")
 rawr_target_folder(uv "extras/libuv")
+
+# libjuice
+rawr_target_folder(juice "extras/juice")
+rawr_target_folder(juice-static "extras/juice")
+rawr_target_folder(juice-tests "extras/juice")
+
+# re
+rawr_target_folder(re "deps")
+
+# playgrounds
+rawr_target_folder(playground_juice "playgrounds")
+rawr_target_folder(playground_lws "playgrounds")
+rawr_target_folder(playground_pa "playgrounds")
+rawr_target_folder(playground_re_sip "playgrounds")
+rawr_target_folder(playground_srtp_recv "playgrounds")
+rawr_target_folder(playground_srtp_send "playgrounds")
+rawr_target_folder(playground_srtp_sendrecv "playgrounds")
+rawr_target_folder(playground_srtp_siprecv "playgrounds")
+rawr_target_folder(playground_strophe "playgrounds")
+rawr_target_folder(playground_ui "playgrounds")
+rawr_target_folder(playground_juice "playgrounds")
