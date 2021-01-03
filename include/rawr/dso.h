1803a23ac5874123c50408020fbbfcc4f210ba40
c6burns <chris@kitty.city>
2021-01-03 12:54:21 -0800

    defined setup and cleanup functions

---
+++ include/rawr/dso.h
@@ -0,0 +1,9 @@
+#ifndef RAWR_DSO_H
+#define RAWR_DSO_H
+
+#include "rawr/platform.h"
+
+RAWR_API int32_t RAWR_CALL rawr_dso_setup();
+RAWR_API int32_t RAWR_CALL rawr_dso_cleanup();
+
+#endif
--- src/dso/dso.c
+++ src/dso/dso.c
@@ -1,2 +1,14 @@
-#include "rawr/platform.h"
+#include "rawr/dso.h"
+
 #include "rawr/error.h"
+
+RAWR_API int32_t RAWR_CALL rawr_dso_setup()
+{
+    return RAWR_OK;
+}
+
+
+RAWR_API int32_t RAWR_CALL rawr_dso_cleanup()
+{
+    return RAWR_OK;
+}
