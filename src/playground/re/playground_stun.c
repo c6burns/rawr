8b9c8f3eae339a2eb874bc44871ac915cac34634
c6burns <chris@kitty.city>
2021-01-15 19:38:29 -0700

    fixed on PS5

--- src/playground/re/playground_stun.c
+++ src/playground/re/playground_stun.c
@@ -1,4 +1,5 @@
 #include "rawr/error.h"
+#include "rawr/net.h"
 #include "rawr/stun.h"
 
 #include "mn/log.h"
@@ -7,26 +8,25 @@
 
 int main(void)
 {
+    int err;
     rawr_Endpoint epStunServ, epExternal;
 
-    RAWR_GUARD_CLEANUP(libre_init());
+    RAWR_GUARD_CLEANUP(err = rawr_Net_Setup());
 
     rawr_Endpoint_SetBytes(&epStunServ, 74, 125, 197, 127, 19302);
 
-    rawr_StunClient_BindingRequest(&epStunServ, &epExternal);
+    RAWR_GUARD_CLEANUP(err = rawr_StunClient_BindingRequest(&epStunServ, &epExternal));
 
     char ipstr[255];
     uint16_t port;
-    rawr_Endpoint_String(&epExternal, &port, ipstr, 255);
+    RAWR_GUARD_CLEANUP(err = rawr_Endpoint_String(&epExternal, &port, ipstr, 255));
     mn_log_info("%s:%u", ipstr, port);
 
-    libre_close();
-
-    return 0;
+    err = 0;
 
 cleanup:
-    mn_log_error("ERROR");
-    libre_close();
+    if (err) mn_log_error("ERROR");
+    rawr_Net_Cleanup();
 
     return 1;
 }
